6a8fbdb0e25a30e32ead6a5f1b3306c4ee3b78e5
Paulo Cunha <pcunha00@hotmail.com>
2025-05-27 18:12:38 -0300

    Implement find one line

--- go2.py
+++ go2.py
@@ -53,18 +53,6 @@ def is_valid (x, y, white_pieces, black_pieces, board_size):
             return True
     return False
 
-def get_bounds(coord_list):
-    xs, ys = zip(*coord_list)
-    return min(xs), max(xs), min(ys), max(ys)
-
-#def get_lines(white_pieces, black_pieces, board_size):
-#    # SCAN BLACK @
-#    color = '@'
-#    min_x, max_x, min_y, max_y = get_bounds(black_pieces)
-#    for x in range(min_x, max_x + 1):
-#        for y in range(min_y, max_y + 1):
-
-
 def update_board(x, y, color, white_pieces, black_pieces, board):
     if color == "WHITE":
         white_pieces.append((x,y))
@@ -82,6 +70,77 @@ def toggle_turn(color):
     if color == "BLACK":
         return "WHITE"
     return "ERROR"
+
+def get_bounds(coord_list):
+    xs, ys = zip(*coord_list)
+    return min(xs), max(xs), min(ys), max(ys)
+
+#def get_lines(white_pieces, black_pieces, board_size):
+#    # SCAN BLACK @
+#    color = '@'
+#    min_x, max_x, min_y, max_y = get_bounds(black_pieces)
+#    for x in range(min_x, max_x + 1):
+#        for y in range(min_y, max_y + 1):
+
+def go_first(x, y, char, board, dx, dy, max_x, max_y):
+    while (board[x][y] != char and x <= max_x and y <= max_y):
+        x += dx
+        y += dy
+    if (board[x][y] != char):
+        x = None
+        y = None
+    return x,y
+
+def go_last(x, y, char, board, dx, dy, max_x, max_y):
+    while (board[x][y] == char and x <= max_x and y <= max_y):
+        x += dx
+        y += dy
+    return x - dx, y - dy
+
+def get_ini(x,y, dx, dy, board):
+    x_ini = x - dx
+    y_ini = y - dy
+    if (x_ini >= 0 and y_ini >= 0 and board[x_ini][y_ini] == '.'):
+        ini = (x_ini, y_ini)
+    else:
+        ini = ()
+    return ini
+
+def get_fin(x,y, dx, dy, board, board_size):
+    x_fin = x + dx
+    y_fin = y + dy
+    if (x_fin <= board_size and y_fin <= board_size and board[x_fin][y_fin] == '.'):
+        fin = (x_fin, y_fin)
+    else:
+        fin = ()
+    return fin
+
+def get_lines(x, y, char, board, dx, dy, max_x, max_y, board_size):
+# faz loop a partir do ponto de partida ate final do bound ou board
+    # vai ate primeria peca
+    xf, yf = go_first(x,y,char, board, dx, dy, max_x, max_y)
+    # pega coordenada da antecessora se tiver
+    ini = get_ini(xf, yf, dx, dy, board)
+    # vai ate ultima peca
+    xl, yl = go_last(xf, yf, char, board, dx, dy, max_x, max_y)
+    # calcula tamanho
+    size = max(xl - xf, yl - yf) + 1
+    # pega coordenada da sucessora se houver
+    fin = get_fin(xl, yl, dx, dy, board, board_size)
+    kind = 2;
+    if not ini:
+        kind -= 1
+    if not fin:
+        kind -= 1
+    dic = {"size":size, "dir":(dx,dy), "kind": kind, "ini":ini, "fin":fin}
+    print(dic)
+    return dic
+
+
+        # monta dic com tamanho, dir, tipo, ini, fin
+    # append dic na lista
+
+
 
 def main():
     # INITIALIZE
@@ -113,8 +172,16 @@ def main():
         #PRINT PIECES
         print("B: ",black_pieces)
         print("W: ",white_pieces)
+        print(get_bounds(black_pieces + white_pieces))
         print(get_bounds(black_pieces))
         print(get_bounds(white_pieces))
+        a, b = go_first(6, 0, '@', board, 0, 1, 10, 10)
+        print(a,b)
+        ini = get_ini(a, b, 0, 1, board)
+        print(ini)
+        print('last')
+        get_lines(6, 0, '@', board, 0, 1, 10, 10, board_size)
+
 
 
 if __name__ == "__main__":
